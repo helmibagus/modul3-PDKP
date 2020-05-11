d64588ec9bef7d571fe4364006ee57ee41f83fac
helmi bagus <helmibagus8@gmail.com>
2020-05-11 12:06:46 +0700

    first commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author HELMI
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application int logic here
+        int i=1;
+        while (i <= 100){
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
